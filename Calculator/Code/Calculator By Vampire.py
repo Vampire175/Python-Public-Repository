fcde2383e83cc9ebd54807cbb73f8e21eb4734fb
Vampire175 <rupalidhakate72@gmail.com>
2024-09-08 13:01:25 +0530

    Add files via upload

---
+++ Calculator/Code/Calculator By Vampire.py
@@ -0,0 +1,23 @@
+a=input("Enter your first number: ")
+b=input("Enter your second number: ")
+c=input("Enter what do you want to do addition,subtraction,multiplication,division: ")
+if c=="addition":
+    print("The answer is",int(a)+int(b))
+elif c=="subtraction":
+    print("The answer is",int(a)-int(b))
+elif c=='multiplication':
+    print("The answer is",int(a)*int(b))
+elif c=='division':
+    print("The answer is",int(a)/int(b))
+if c=="+":
+    print("The answer is",int(a)+int(b))
+elif c=="-":
+    print("The answer is",int(a)-int(b))
+elif c=='*':
+    print("The answer is",int(a)*int(b))
+elif c=='/':
+    print("The answer is",int(a)/int(b))
+else:
+    print("It's the wrong input")
+print('Made By Vampire in India with ❤️')
+d=input('Type enter to exit the programm ')
