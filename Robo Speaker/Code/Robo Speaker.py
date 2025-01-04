9b95c96d8cd77234f8188348518651c1ccdbe8fc
Vampire <rupalidhakate72@gmail.com>
2025-01-04 21:33:52 +0530

    Update Robo Speaker.py

--- Robo Speaker/Code/Robo Speaker.py
+++ Robo Speaker/Code/Robo Speaker.py
@@ -1,10 +1,25 @@
 import win32com.client
-
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
-
+from gtts import gTTS
+import os
+print('Made by Arjun Fanibahre')
 while True:
-    a = input('What do you want to speak (type "exit" to close): ')
+    a = input("1) Male Voice\n2) Female Voice\nEnter the number (type 'exit' to close): ")
     if a.lower() == "exit":
-        print("Exiting the program.\nMade by Vampire with ❤️")
+        
         break
-    speaker.Speak(a)
+    
+    b = input('What do you want to speak (type "exit" to close): ')
+    if b.lower() == "exit":
+        
+        break
+
+    if a == "1":
+        speaker = win32com.client.Dispatch("SAPI.SpVoice")
+        speaker.Speak(b)
+    elif a == "2":
+        speech = gTTS(b)
+        speech.save('audio.mp3')
+        os.system('start audio.mp3')
+    else:
+        print("Invalid option. Please choose 1 for Male Voice or 2 for Female Voice.")
+
